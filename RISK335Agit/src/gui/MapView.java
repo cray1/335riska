77bd706f718fb8b8fdc1a2b84237bb660913cebb
Aj <ajvenne@email.arizona.edu>
2011-11-30 00:41:10 -0700

    Territory names have been updated, based on Steve's risk board. If anyone sees an error let me know.

--- RISK335Agit/src/gui/MapView.java
+++ RISK335Agit/src/gui/MapView.java
@@ -1,7 +1,6 @@
 package gui;
 
 import java.awt.Graphics;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -35,10 +34,11 @@ public class MapView extends MasterViewPanel{
 	HashMap<String, Territory> eur;
 	HashMap<String, Territory> aus;
 	HashMap<String, Territory> asia;
-	
+	Image mapImage;
 	public MapView(MasterView m) {
 		super(m);
 		setUpGUI();
+		
 		newGame = new Game();
 		gameMap = newGame.getMap();
 		continents = gameMap.getMap();
@@ -53,10 +53,10 @@ public class MapView extends MasterViewPanel{
 		this.setVisible(true);
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
-		Image mapImage = new ImageIcon("images/map.png").getImage();
+		mapImage = new ImageIcon("images/map.png").getImage();
 		buffImage = new BufferedImage(mapImage.getWidth(null),mapImage.getHeight(null),BufferedImage.TYPE_INT_ARGB);
-
-//		Image whiteFlag = new ImageIcon("images/whiteFlag.png").getImage();
+		
+		
 		Graphics g = buffImage.getGraphics();
 	    g.drawImage(mapImage, 0, 0, null);
 
@@ -64,6 +64,7 @@ public class MapView extends MasterViewPanel{
 		this.addMouseListener(new mouse());
 		//Adds motion listener for tooltip text
 		this.addMouseMotionListener(new mouseMove());
+		
 		
 		//NOTE
 		//Currently a bug that needs to be worked out: Buffered Image and displayed image are not in exact spot when this is added, hard to track since
@@ -83,6 +84,8 @@ public class MapView extends MasterViewPanel{
 	}
 	public void paintComponent(Graphics g){
 		
+		mapImage = mapImage.getScaledInstance(this.getWidth(), this.getHeight(), 2);
+
 	}
 	private class mouse implements MouseListener{
 
@@ -150,15 +153,15 @@ public class MapView extends MasterViewPanel{
 		}
 		//Area 2
 		if(buffImage.getRGB(x, y) == -256 && x < (buffImage.getWidth() / 4.066666667)){
-			return "North America Area 2";
+			return "Alberta";
 		}
 		//Area 3
 		if(buffImage.getRGB(x, y) == -128 && y > (buffImage.getHeight() / 3.4)){
-			return "North America Area 3";
+			return "Central America";
 		}
 		//Area 4
 		if(buffImage.getRGB(x, y) == -8355840 && y > (buffImage.getHeight() / 5.3125)){
-			return "North America Area 4";
+			return "Eastern United States";
 			
 		}
 		//Area 5
@@ -168,160 +171,161 @@ public class MapView extends MasterViewPanel{
 		}
 		//Area 6
 		if(buffImage.getRGB(x, y) == -11513817 && y < (buffImage.getHeight() / 5.3125)){
-			return "North America Area 6";
+			return "Northwest Territory";
 		}
 		//Area 7
 		if(buffImage.getRGB(x, y) == -7039927){
-			return "North America Area 7";
+			return "Ontario";
 		}
 		//Area 8
 		if(buffImage.getRGB(x, y) == -128 && y < (buffImage.getHeight() / 3.4)){
-			return "North America Area 8";
+			return "Quebec";
 		}
 		//Area 9
 		if(buffImage.getRGB(x, y) == -11513817 && y >(buffImage.getHeight() / 5.3125)){
-			return "North America Area 9";
+			return "Western United States";
 		}
 		
 		//SOUTH AMERICA
 		//Area 1
 		if(buffImage.getRGB(x, y) == -65536){
-			return "South America Area 1";
+			return "Argentina";
 		}
 		//Area 2
 		if(buffImage.getRGB(x, y) == -8372160){
-			return "South America Area 2";
+			return "Brazil";
 		}
 		//Area 3
 		if(buffImage.getRGB(x, y) == -8388608){
-			return "South America Area 3";
+			return "Peru";
 		}
 		//Area 4
 		if(buffImage.getRGB(x, y) == -32640){
-			return "South America Area 4";
+			return "Venezuela";
 		}
 		
 		//AUSTRALIA
 		//Area 1
 		if(buffImage.getRGB(x, y) == -12582848){
-			return "Australia Area 1";
+			return "Eastern Australia";
 		}
 		//Area 2
 		if(buffImage.getRGB(x, y) == -8388353){
-			return "Australia Area 2";
+			return "Indonesia";
 		}
 		//Area 3
 		if(buffImage.getRGB(x, y) == -65281){
-			return "Australia Area 3";
+			return "New Guinea";
 		}
 		//Area 4
 		if(buffImage.getRGB(x, y) == -8388544){
-			return "Australia Area 4";
+			return "Western Australia";
 		}
 		
 		//AFRICA
 		//Area 1
 		if(buffImage.getRGB(x, y) == -5351680 && x < (buffImage.getWidth() / 1.694444444)){
-			return "Africa Area 1";
+			return "Congo";
 		}
 		//Area 2
 		if(buffImage.getRGB(x, y) == -32768){
-			return "Africa Area 2";
+			return "East Africa";
 		}
 		//Area 3
 		if(buffImage.getRGB(x, y) == -8372224 && y < (buffImage.getHeight() / 2.266666667)){
-			return "Africa Area 3";
+			return "Egypt";
 		}
 		//Area 4
 		if(buffImage.getRGB(x, y) == -5351680 && x > (buffImage.getWidth() / 1.694444444)){
-			return "Africa Area 4";
+			return "Madagascar";
 		}
 		//Area 5
 		if(buffImage.getRGB(x, y) == -28325){
-			return "Africa Area 5";
+			return "North Africa";
 		}
 		//Area 6
 		if(buffImage.getRGB(x, y) == -8372224 && y > (buffImage.getHeight() / 2.266666667)){
-			return "Africa Area 6";
+			return "South Africa";
 		}
+		
 		//EUROPE
 		//Area 1
 		if(buffImage.getRGB(x, y) == -16760704 && y < (buffImage.getHeight() / 4.857142857)){
-			return "Europe Area 1";
+			return "Great Britain";
 		}
 		//Area 2
 		if(buffImage.getRGB(x, y) == -16776961 && y < (buffImage.getHeight() / 7.555555556)){
-			return "Europe Area 2";
+			return "Iceland";
 		}
 		//Area 3
 		if(buffImage.getRGB(x, y) == -16776961 && y > (buffImage.getHeight() / 7.555555556)){
-			return "Europe Area 3";
+			return "Northern Europe";
 		}
 		//Area 4
 		if(buffImage.getRGB(x, y) == -16744193 && y < (buffImage.getHeight() / 5.666666667)){
-			return "Europe Area 4";
+			return "Scandanavia";
 		}
 		//Area 5
 		if(buffImage.getRGB(x, y) == -16760704 && y > (buffImage.getHeight() / 4.857142857)){
-			return "Europe Area 5";
+			return "Southern Europe";
 		}
 		//Area 6
 		if(buffImage.getRGB(x, y) == -16777088){
-			return "Europe Area 6";
+			return "Ukraine";
 		}
 		//Area 7
 		if(buffImage.getRGB(x, y) == -16744193 && y > (buffImage.getHeight() / 5.666666667)){
-			return "Europe Area 7";
+			return "Western Europe";
 		}
 		
 		//ASIA
 		//Area 1
 		if(buffImage.getRGB(x, y) == -8323200 && x < (buffImage.getWidth() / 1.355555556)){
-			return "Asia Area 1";
+			return "Afghanistan";
 		}
 		//Area 2
 		if(buffImage.getRGB(x, y) == -16744384 && y > (buffImage.getHeight() / 4.4)){
-			return "Asia Area 2";
+			return "China";
 		}
 		//Area 3
 		if(buffImage.getRGB(x, y) == -16744320 && y > (buffImage.getHeight() / 4.689655172)){
-			return "Asia Area 3";
+			return "India";
 		}
 		//Area 4
 		if(buffImage.getRGB(x, y) == -8323328 && x < (buffImage.getWidth() / 1.167464115)){
-			return "Asia Area 4";
+			return "Irkutsk";
 		}
 		//Area 5
 		if(buffImage.getRGB(x, y) == -8323328 && x > (buffImage.getWidth() / 1.167464115)){
-			return "Asia Area 5";
+			return "Japan";
 		}
 		//Area 6
 		if(buffImage.getRGB(x, y) == -16744384 && y < (buffImage.getHeight() / 4.657534247)){
-			return "Asia Area 6";
+			return "Kamchatka";
 		}
 		//Area 7
 		if(buffImage.getRGB(x, y) == -16744448 && y > (buffImage.getHeight() / 4)){
-			return "Asia Area 7";
+			return "Middle East";
 		}
 		//Area 8
 		if(buffImage.getRGB(x, y) == -16760832 && x > (buffImage.getWidth() / 1.384790011)){
-			return "Asia Area 8";
+			return "Mongolia";
 		}
 		//Area 9
 		if(buffImage.getRGB(x, y) == -8323200 && x > (buffImage.getWidth() / 1.355555556)){
-			return "Asia Area 9";
+			return "Siam";
 		}
 		//Area 10
 		if(buffImage.getRGB(x, y) == -16744448 && y < (buffImage.getHeight() / 4)){
-			return "Asia Area 10";
+			return "Siberia";
 		}
 		//Area 11
 		if(buffImage.getRGB(x, y) == -16760832 && x < (buffImage.getWidth() / 1.384790011)){
-			return "Asia Area 11";
+			return "Ural";
 		}
 		//Area 12
 		if(buffImage.getRGB(x, y) == -16744320 && y < (buffImage.getHeight() / 4.689655172)){
-			return "Asia Area 12";
+			return "Yakutsk";
 		}
 		
 		return null;
