3d61325217b9a7bc7a12ab55086512a3be9c9cde
panzercannon01 <cray1@email.arizona.edu>
2011-12-01 01:13:10 -0700

    merge

--- RISK335Agit/src/gui/MapView.java
+++ RISK335Agit/src/gui/MapView.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 
+
 import javax.swing.JLabel;
 
 import baseModel.Continent;
@@ -20,14 +21,9 @@ import baseModel.Player;
 import baseModel.Team;
 import baseModel.Territory;
 
-/**
- * @author AJ Venne
- */
-public class MapView extends MasterViewPanel {
-	/**
-	 * @author AJ Venne
-	 */
-	private static final long serialVersionUID = 5977780590454654721L;
+
+
+public class MapView extends MasterViewPanel{
 	BufferedImage buffImage;
 	Game newGame;
 	Map gameMap;
@@ -40,53 +36,47 @@ public class MapView extends MasterViewPanel {
 	HashMap<String, Territory> asia;
 	HashMap<String, Territory> territories;
 	Image mapImage;
-
-	/**
-	 * @author AJ Venne
-	 */
 	public MapView(MasterView m) {
 		super(m);
 		setUpGUI();
 		setUpGame();
-
+		
 	}
-
-	private void setUpGUI() {
+	private void setUpGUI(){
 		this.setVisible(true);
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-		mapImage = new ImageIcon("images/map.png").getImage();
-		buffImage = new BufferedImage(mapImage.getWidth(null),
-				mapImage.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-
+		
+		mapImage = new ImageIcon("images/map2.png").getImage();
+		buffImage = new BufferedImage(mapImage.getWidth(null),mapImage.getHeight(null),BufferedImage.TYPE_INT_ARGB);
+		
+		
 		Graphics g = buffImage.getGraphics();
-		g.drawImage(mapImage, 0, 0, null);
+	    g.drawImage(mapImage, 0, 0, null);
 
-		this.add(new JLabel(new ImageIcon(buffImage)));
+	    this.add(new JLabel(new ImageIcon(buffImage)));
 		this.addMouseListener(new mouse());
-		// Adds motion listener for tooltip text
+		//Adds motion listener for tooltip text
 		this.addMouseMotionListener(new mouseMove());
-
-		// NOTE
-		// Currently a bug that needs to be worked out: Buffered Image and
-		// displayed image are not in exact spot when this is added, hard to
-		// track since
-		// the buffered is invisible...
-
-		// adding a user bar for cards, buttons, etc
-		// JPanel userBar = new JPanel();
-		// this.add(userBar);
-		// userBar.setLocation(0,680);
-		// userBar.setVisible(true);
-		// //adding buttons
-		// JButton cards = new JButton("Cards");
-		// JButton quit = new JButton("Quit Game");
-		// userBar.add(cards);
-		// userBar.add(quit);
-
+		
+		
+		//NOTE
+		//Currently a bug that needs to be worked out: Buffered Image and displayed image are not in exact spot when this is added, hard to track since
+		//the buffered is invisible...
+		
+		//adding a user bar for cards, buttons, etc
+//		JPanel userBar = new JPanel();
+//		this.add(userBar);
+//		userBar.setLocation(0,680);
+//		userBar.setVisible(true);
+//		//adding buttons
+//		JButton cards = new JButton("Cards");
+//		JButton quit = new JButton("Quit Game");
+//		userBar.add(cards);
+//		userBar.add(quit);
+		
 	}
-
-	private void setUpGame() {
+	
+	private void setUpGame(){
 		newGame = new Game();
 		gameMap = newGame.getMap();
 		continents = gameMap.getMap();
@@ -98,253 +88,257 @@ public class MapView extends MasterViewPanel {
 		aus = continents.get("Australia").getChildrenAsHashMap();
 		asia = continents.get("Asia").getChildrenAsHashMap();
 	}
-
-	@Override
-	public void paintComponent(Graphics g) {
-
-		// mapImage = mapImage.getScaledInstance(this.getWidth(),
-		// this.getHeight(), 2);
+	
+	public void paintComponent(Graphics g){
+		
+//		mapImage = mapImage.getScaledInstance(this.getWidth(), this.getHeight(), 2);
 
 	}
-
-	/**
-	 * @author AJ Venne
-	 */
-	private class mouse implements MouseListener {
+	private class mouse implements MouseListener{
 
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			if ((e.getX() < buffImage.getWidth())
-					&& (e.getY() < buffImage.getHeight()))
-				System.out.println(territories.get(
-						getLocation(e.getX(), e.getY())).toString());
+			if(e.getX() < buffImage.getWidth() && e.getY() < buffImage.getHeight()){
+//			System.out.println(e.getX() + "," + e.getY() + " COLOR: " + buffImage.getRGB(e.getX(), e.getY()));
+			//calls helper method designed to check which area is being clicked based on coordinates and color
+//			System.out.println(getLocation(e.getX(), e.getY()));
+						
+			System.out.println(territories.get(getLocation(e.getX(), e.getY())).toString());
+			}
 		}
 
 		@Override
 		public void mouseEntered(MouseEvent e) {
-
+	
 		}
 
 		@Override
 		public void mouseExited(MouseEvent e) {
 			// TODO Auto-generated method stub
-
+			
 		}
 
 		@Override
 		public void mousePressed(MouseEvent e) {
 			// TODO Auto-generated method stub
-
+		
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent e) {
 			// TODO Auto-generated method stub
-
+			
 		}
-
+		
 	}
-
-	/**
-	 * @author AJ Venne
-	 */
-	private class mouseMove implements MouseMotionListener {
+	private class mouseMove implements MouseMotionListener{
 
 		@Override
 		public void mouseDragged(MouseEvent arg0) {
-
+		
 		}
 
 		@Override
 		public void mouseMoved(MouseEvent arg0) {
-			// checking which territory, for use with tool tip which will later
-			// display owner, etc. info
-			// NORTH AMERICA
-			if ((arg0.getX() < buffImage.getWidth())
-					&& (arg0.getY() < buffImage.getHeight()))
+			//checking which territory, for use with tool tip which will later display owner, etc. info
+			//NORTH AMERICA
+			if(arg0.getX() < buffImage.getWidth() && arg0.getY() < buffImage.getHeight()){
 				setToolTipText(getLocation(arg0.getX(), arg0.getY()));
-			else
+			}
+			else{
 				setToolTipText(null);
+			}
 		}
-
+		
 	}
+	//Helper method mentioned above for determining location
+	private String getLocation(int x, int y){
+		
+		//NORTH AMERICA
+		//Area 1
+		if(buffImage.getRGB(x, y) == -8355840 && y < (buffImage.getHeight() / 5.3125)){
 
-	/**
-	 * @author AJ Venne
-	 */
-	// Helper method mentioned above for determining location
-	private String getLocation(int x, int y) {
-
-		// NORTH AMERICA
-		// Area 1
-		if ((buffImage.getRGB(x, y) == -8355840)
-				&& (y < (buffImage.getHeight() / 5.3125)))
-			return "Alaska"; // + na.get("Alaska").getOwner().getTeam();
-		// Area 2
-		if ((buffImage.getRGB(x, y) == -256)
-				&& (x < (buffImage.getWidth() / 4.066666667)))
+			return "Alaska";  //+ na.get("Alaska").getOwner().getTeam();
+		}
+		//Area 2
+		if(buffImage.getRGB(x, y) == -256 && x < (buffImage.getWidth() / 4.066666667)){
 			return "Alberta";
-		// Area 3
-		if ((buffImage.getRGB(x, y) == -128)
-				&& (y > (buffImage.getHeight() / 3.4)))
+		}
+		//Area 3
+		if(buffImage.getRGB(x, y) == -128 && y > (buffImage.getHeight() / 3.4)){
 			return "Central America";
-		// Area 4
-		if ((buffImage.getRGB(x, y) == -8355840)
-				&& (y > (buffImage.getHeight() / 5.3125)))
+		}
+		//Area 4
+		if(buffImage.getRGB(x, y) == -8355840 && y > (buffImage.getHeight() / 5.3125)){
 			return "Eastern United States";
-		// Area 5
-		if ((buffImage.getRGB(x, y) == -256)
-				&& (x > (buffImage.getWidth() / 4.066666667)))
+			
+		}
+		//Area 5
+		if(buffImage.getRGB(x, y) == -256 && x >  (buffImage.getWidth() / 4.066666667)){
 			return "Greenland";
-		// Area 6
-		if ((buffImage.getRGB(x, y) == -11513817)
-				&& (y < (buffImage.getHeight() / 5.3125)))
+		}
+		//Area 6
+		if(buffImage.getRGB(x, y) == -11513817 && y < (buffImage.getHeight() / 5.3125)){
 			return "Northwest Territory";
-		// Area 7
-		if (buffImage.getRGB(x, y) == -7039927)
+		}
+		//Area 7
+		if(buffImage.getRGB(x, y) == -7039927){
 			return "Ontario";
-		// Area 8
-		if ((buffImage.getRGB(x, y) == -128)
-				&& (y < (buffImage.getHeight() / 3.4)))
+		}
+		//Area 8
+		if(buffImage.getRGB(x, y) == -128 && y < (buffImage.getHeight() / 3.4)){
 			return "Quebec";
-		// Area 9
-		if ((buffImage.getRGB(x, y) == -11513817)
-				&& (y > (buffImage.getHeight() / 5.3125)))
+		}
+		//Area 9
+		if(buffImage.getRGB(x, y) == -11513817 && y >(buffImage.getHeight() / 5.3125)){
 			return "Western United States";
-
-		// SOUTH AMERICA
-		// Area 1
-		if (buffImage.getRGB(x, y) == -65536)
+		}
+		
+		//SOUTH AMERICA
+		//Area 1
+		if(buffImage.getRGB(x, y) == -65536){
 			return "Argentina";
-		// Area 2
-		if (buffImage.getRGB(x, y) == -8372160)
+		}
+		//Area 2
+		if(buffImage.getRGB(x, y) == -8372160){
 			return "Brazil";
-		// Area 3
-		if (buffImage.getRGB(x, y) == -8388608)
+		}
+		//Area 3
+		if(buffImage.getRGB(x, y) == -8388608){
 			return "Peru";
-		// Area 4
-		if (buffImage.getRGB(x, y) == -32640)
+		}
+		//Area 4
+		if(buffImage.getRGB(x, y) == -32640){
 			return "Venezuela";
-
-		// AUSTRALIA
-		// Area 1
-		if (buffImage.getRGB(x, y) == -12582848)
+		}
+		
+		//AUSTRALIA
+		//Area 1
+		if(buffImage.getRGB(x, y) == -12582848){
 			return "Eastern Australia";
-		// Area 2
-		if (buffImage.getRGB(x, y) == -8388353)
+		}
+		//Area 2
+		if(buffImage.getRGB(x, y) == -8388353){
 			return "Indonesia";
-		// Area 3
-		if (buffImage.getRGB(x, y) == -65281)
+		}
+		//Area 3
+		if(buffImage.getRGB(x, y) == -65281){
 			return "New Guinea";
-		// Area 4
-		if (buffImage.getRGB(x, y) == -8388544)
+		}
+		//Area 4
+		if(buffImage.getRGB(x, y) == -8388544){
 			return "Western Australia";
-
-		// AFRICA
-		// Area 1
-		if ((buffImage.getRGB(x, y) == -5351680)
-				&& (x < (buffImage.getWidth() / 1.694444444)))
+		}
+		
+		//AFRICA
+		//Area 1
+		if(buffImage.getRGB(x, y) == -5351680 && x < (buffImage.getWidth() / 1.694444444)){
 			return "Congo";
-		// Area 2
-		if (buffImage.getRGB(x, y) == -32768)
+		}
+		//Area 2
+		if(buffImage.getRGB(x, y) == -32768){
 			return "East Africa";
-		// Area 3
-		if ((buffImage.getRGB(x, y) == -8372224)
-				&& (y < (buffImage.getHeight() / 2.266666667)))
+		}
+		//Area 3
+		if(buffImage.getRGB(x, y) == -8372224 && y < (buffImage.getHeight() / 2.266666667)){
 			return "Egypt";
-		// Area 4
-		if ((buffImage.getRGB(x, y) == -5351680)
-				&& (x > (buffImage.getWidth() / 1.694444444)))
+		}
+		//Area 4
+		if(buffImage.getRGB(x, y) == -5351680 && x > (buffImage.getWidth() / 1.694444444)){
 			return "Madagascar";
-		// Area 5
-		if (buffImage.getRGB(x, y) == -28325)
+		}
+		//Area 5
+		if(buffImage.getRGB(x, y) == -28325){
 			return "North Africa";
-		// Area 6
-		if ((buffImage.getRGB(x, y) == -8372224)
-				&& (y > (buffImage.getHeight() / 2.266666667)))
+		}
+		//Area 6
+		if(buffImage.getRGB(x, y) == -8372224 && y > (buffImage.getHeight() / 2.266666667)){
 			return "South Africa";
-
-		// EUROPE
-		// Area 1
-		if ((buffImage.getRGB(x, y) == -16760704)
-				&& (y < (buffImage.getHeight() / 4.857142857)))
+		}
+		
+		//EUROPE
+		//Area 1
+		if(buffImage.getRGB(x, y) == -16760704 && y < (buffImage.getHeight() / 4.857142857)){
 			return "Great Britain";
-		// Area 2
-		if ((buffImage.getRGB(x, y) == -16776961)
-				&& (y < (buffImage.getHeight() / 7.555555556)))
+		}
+		//Area 2
+		if(buffImage.getRGB(x, y) == -16776961 && y < (buffImage.getHeight() / 7.555555556)){
 			return "Iceland";
-		// Area 3
-		if ((buffImage.getRGB(x, y) == -16776961)
-				&& (y > (buffImage.getHeight() / 7.555555556)))
+		}
+		//Area 3
+		if(buffImage.getRGB(x, y) == -16776961 && y > (buffImage.getHeight() / 7.555555556)){
 			return "Northern Europe";
-		// Area 4
-		if ((buffImage.getRGB(x, y) == -16744193)
-				&& (y < (buffImage.getHeight() / 5.666666667)))
+		}
+		//Area 4
+		if(buffImage.getRGB(x, y) == -16744193 && y < (buffImage.getHeight() / 5.666666667)){
 			return "Scandinavia";
-		// Area 5
-		if ((buffImage.getRGB(x, y) == -16760704)
-				&& (y > (buffImage.getHeight() / 4.857142857)))
+		}
+		//Area 5
+		if(buffImage.getRGB(x, y) == -16760704 && y > (buffImage.getHeight() / 4.857142857)){
 			return "Southern Europe";
-		// Area 6
-		if (buffImage.getRGB(x, y) == -16777088)
+		}
+		//Area 6
+		if(buffImage.getRGB(x, y) == -16777088){
 			return "Ukraine";
-		// Area 7
-		if ((buffImage.getRGB(x, y) == -16744193)
-				&& (y > (buffImage.getHeight() / 5.666666667)))
+		}
+		//Area 7
+		if(buffImage.getRGB(x, y) == -16744193 && y > (buffImage.getHeight() / 5.666666667)){
 			return "Western Europe";
-
-		// ASIA
-		// Area 1
-		if ((buffImage.getRGB(x, y) == -8323200)
-				&& (x < (buffImage.getWidth() / 1.355555556)))
+		}
+		
+		//ASIA
+		//Area 1
+		if(buffImage.getRGB(x, y) == -8323200 && x < (buffImage.getWidth() / 1.355555556)){
 			return "Afghanistan";
-		// Area 2
-		if ((buffImage.getRGB(x, y) == -16744384)
-				&& (y > (buffImage.getHeight() / 4.4)))
+		}
+		//Area 2
+		if(buffImage.getRGB(x, y) == -16744384 && y > (buffImage.getHeight() / 4.4)){
 			return "China";
-		// Area 3
-		if ((buffImage.getRGB(x, y) == -16744320)
-				&& (y > (buffImage.getHeight() / 4.689655172)))
+		}
+		//Area 3
+		if(buffImage.getRGB(x, y) == -16744320 && y > (buffImage.getHeight() / 4.689655172)){
 			return "India";
-		// Area 4
-		if ((buffImage.getRGB(x, y) == -8323328)
-				&& (x < (buffImage.getWidth() / 1.167464115)))
+		}
+		//Area 4
+		if(buffImage.getRGB(x, y) == -8323328 && x < (buffImage.getWidth() / 1.167464115)){
 			return "Irkutsk";
-		// Area 5
-		if ((buffImage.getRGB(x, y) == -8323328)
-				&& (x > (buffImage.getWidth() / 1.167464115)))
+		}
+		//Area 5
+		if(buffImage.getRGB(x, y) == -8323328 && x > (buffImage.getWidth() / 1.167464115)){
 			return "Japan";
-		// Area 6
-		if ((buffImage.getRGB(x, y) == -16744384)
-				&& (y < (buffImage.getHeight() / 4.657534247)))
+		}
+		//Area 6
+		if(buffImage.getRGB(x, y) == -16744384 && y < (buffImage.getHeight() / 4.657534247)){
 			return "Kamchatka";
-		// Area 7
-		if ((buffImage.getRGB(x, y) == -16744448)
-				&& (y > (buffImage.getHeight() / 4)))
+		}
+		//Area 7
+		if(buffImage.getRGB(x, y) == -16744448 && y > (buffImage.getHeight() / 4)){
 			return "Middle East";
-		// Area 8
-		if ((buffImage.getRGB(x, y) == -16760832)
-				&& (x > (buffImage.getWidth() / 1.384790011)))
+		}
+		//Area 8
+		if(buffImage.getRGB(x, y) == -16760832 && x > (buffImage.getWidth() / 1.384790011)){
 			return "Mongolia";
-		// Area 9
-		if ((buffImage.getRGB(x, y) == -8323200)
-				&& (x > (buffImage.getWidth() / 1.355555556)))
+		}
+		//Area 9
+		if(buffImage.getRGB(x, y) == -8323200 && x > (buffImage.getWidth() / 1.355555556)){
 			return "Siam";
-		// Area 10
-		if ((buffImage.getRGB(x, y) == -16744448)
-				&& (y < (buffImage.getHeight() / 4)))
+		}
+		//Area 10
+		if(buffImage.getRGB(x, y) == -16744448 && y < (buffImage.getHeight() / 4)){
 			return "Siberia";
-		// Area 11
-		if ((buffImage.getRGB(x, y) == -16760832)
-				&& (x < (buffImage.getWidth() / 1.384790011)))
+		}
+		//Area 11
+		if(buffImage.getRGB(x, y) == -16760832 && x < (buffImage.getWidth() / 1.384790011)){
 			return "Ural";
-		// Area 12
-		if ((buffImage.getRGB(x, y) == -16744320)
-				&& (y < (buffImage.getHeight() / 4.689655172)))
+		}
+		//Area 12
+		if(buffImage.getRGB(x, y) == -16744320 && y < (buffImage.getHeight() / 4.689655172)){
 			return "Yakutsk";
-
+		}
+		
 		return "";
-
+		
 	}
+	
 
 }
--- RISK335Agit/src/gui/MasterView.java
+++ RISK335Agit/src/gui/MasterView.java
@@ -3,8 +3,11 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -14,43 +17,34 @@ import javax.swing.JPanel;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 
-/**
- * @author AJ Venne Created on 12:35:37 AM Nov 27, 2011
- */
+
 @SuppressWarnings("serial")
 public class MasterView extends javax.swing.JFrame {
 	private JPanel body = new JPanel();
-
-	/**
-	 * @author AJ Venne Created on 12:35:37 AM Nov 27, 2011
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	
+	public static void main (String[] args){
 		MasterView mv = new MasterView();
 		mv.setVisible(true);
 	}
-
-	/**
-	 * @author AJ Venne Created on 12:35:37 AM Nov 27, 2011
-	 */
-	public MasterView() {
+	
+	public MasterView(){
 		setUpGUICode();
 		setUpMenuBar();
 		setUpBody();
 		changeView(Views.TITLE, null);
 	}
-
-	private void setUpGUICode() {
-		this.setSize(1220, 980); // 680
-		// this.setResizable(false);
-		setLocation(10, 10);
+	Image mapImage = new ImageIcon("images/map2.png").getImage();
+	private void setUpGUICode(){
+		this.setSize(mapImage.getWidth(null), mapImage.getHeight(null) + 80); //680
+//		this.setResizable(false);
+		setLocation(10,10);
 		setTitle("Risk");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
 		add(body, BorderLayout.CENTER);
 	}
-
-	private void setUpMenuBar() {
+	
+	private void setUpMenuBar(){
 		JMenuBar menu = new JMenuBar();
 		JMenu file = new JMenu("File");
 		JMenu about = new JMenu("About");
@@ -62,83 +56,71 @@ public class MasterView extends javax.swing.JFrame {
 		exit.addActionListener(new exitListener());
 		setJMenuBar(menu);
 	}
-
-	private class exitListener implements ActionListener {
-		@Override
+	
+	private class exitListener implements ActionListener{
 		public void actionPerformed(ActionEvent exitClick) {
 			System.exit(0);
 		}
 	}
-
-	private class aboutListener implements MenuListener {
+	private class aboutListener implements MenuListener{
 
 		@Override
 		public void menuCanceled(MenuEvent arg0) {
 			// TODO Auto-generated method stub
-
+			
 		}
 
 		@Override
 		public void menuDeselected(MenuEvent e) {
 			// TODO Auto-generated method stub
-
+			
 		}
 
 		@Override
 		public void menuSelected(MenuEvent e) {
-			JOptionPane
-					.showMessageDialog(
-							null,
-							"Risk \nCreated by:\nSteven Brown\nChristopher Ray\nRyan Robinson\nAndrew Venne - GUI");
-
+			JOptionPane.showMessageDialog(null, "Risk \nCreated by:\nStephen Brown\nChristopher Ray\nRyan Robinson\nAndrew Venne - GUI");
+			
 		}
-
+		
 	}
-
 	private CardLayout card = new CardLayout();
-
-	private void setUpBody() {
+	private void setUpBody(){
 		body.setVisible(true);
 		body.setLayout(card);
 	}
-
-	/**
-	 * @author AJ Venne Created on 12:35:37 AM Nov 27, 2011
-	 * @param v
-	 * @param o
-	 */
-	public void changeView(Views v, Object o) {
-		if (v == Views.BATTLE) {
-			JPanel battleView = new JPanel();
-
+	
+	public void changeView(Views v, Object o){
+		if(v == Views.BATTLE){
+			JPanel battleView  = new JPanel();
+			
 			battleView.setVisible(true);
 			body.add(battleView, "Battle");
-
+			
 			card.show(body, "Battle");
-
+			
 			body.getComponent(0).requestFocusInWindow();
 		}
-		if (v == Views.MAP) {
-			JPanel mapView = new JPanel();
-
+		if(v == Views.MAP){
+			JPanel mapView  = new JPanel();
+			
 			mapView.setVisible(true);
 			mapView.add(new MapView(this));
 			body.add(mapView, "Map");
-
+			
 			card.show(body, "Map");
-
+			
 			body.getComponent(0).requestFocusInWindow();
 		}
-		if (v == Views.TITLE) {
+		if(v == Views.TITLE){
 			card.show(body, "Title");
-
+			
 			JPanel titleView = new JPanel();
 			titleView.setVisible(true);
 			titleView.add(new TitleView(this));
 			titleView.setLayout(new GridLayout());
 			body.add(titleView, "Title");
-
+			
 		}
 	}
-
+	
 }
