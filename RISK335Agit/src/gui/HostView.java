234ae8681ecbcb632c804293155eb10edc6da646
panzercannon01 <cray1@email.arizona.edu>
2011-12-02 07:32:30 -0700

    AJ's changes

---
+++ RISK335Agit/src/gui/HostView.java
@@ -0,0 +1,14 @@
+package gui;
+
+
+
+public class HostView extends MasterViewPanel {
+
+	public HostView(MasterView m) {
+		super(m);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
---
+++ RISK335Agit/src/gui/JoinView.java
@@ -0,0 +1,12 @@
+package gui;
+
+
+
+public class JoinView extends MasterViewPanel {
+
+	public JoinView(MasterView m) {
+		super(m);
+		// TODO Auto-generated constructor stub
+	}
+
+}
