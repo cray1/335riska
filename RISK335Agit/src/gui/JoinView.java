3a519ab3f0b7358b246a2ba59305afe6d4758569
Ryan Robinson <rrobinso@email.arizona.edu>
2011-12-02 14:49:50 -0700

    starting the layout

--- RISK335Agit/src/gui/JoinView.java
+++ RISK335Agit/src/gui/JoinView.java
@@ -1,5 +1,11 @@
 package gui;
 
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
 
 
 public class JoinView extends MasterViewPanel {
@@ -7,6 +13,14 @@ public class JoinView extends MasterViewPanel {
 	public JoinView(MasterView m) {
 		super(m);
 		// TODO Auto-generated constructor stub
+		
 	}
-
+	private void setupLayout(){
+		this.setLayout(new GridLayout(1,2));
+		JPanel left = new JPanel();
+		JPanel right = new JPanel();
+		right.setLayout(new BoxLayout(right, BoxLayout.PAGE_AXIS));
+		JTextArea log = new JTextArea();
+		
+	}
 }
